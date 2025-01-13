158316b311e7ed1ca28db030b8212e5129e3ff79
decodedfaith <komolafefaith@gmail.com>
2025-01-13 14:52:24 +0100

    Initial Commit

---
+++ MintynClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MintynClone
+//
+//  Created by user on 1/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MintynCloneTests/MintynCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  MintynCloneTests.swift
+//  MintynCloneTests
+//
+//  Created by user on 1/13/25.
+//
+
+import Testing
+@testable import MintynClone
+
+struct MintynCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
