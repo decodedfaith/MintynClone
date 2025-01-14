55ba05a50f3993e493c499e583ff2f32135c59cb
decodedfaith <komolafefaith@gmail.com>
2025-01-14 23:56:45 +0100

    -Initial commit, App setup and

---
+++ MintynClone/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  MintynClone
+//
+//  Created by user on 1/14/25.
+//
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBAction func LoginButtonTapped(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
--- MintynClone/ViewController.swift
+++ MintynClone/ViewController.swift
@@ -9,10 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToLoginView", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let loginButton = UIButton(type: .system)
+               loginButton.setTitle("Login", for: .normal)
+               loginButton.setTitleColor(.white, for: .normal)
+               loginButton.layer.cornerRadius = 10
+               loginButton.clipsToBounds = true
+               addGradientToButton(button: loginButton)
+               view.addSubview(loginButton)
+               
+               // Layout constraints for the button (example)
+               loginButton.translatesAutoresizingMaskIntoConstraints = false
+               NSLayoutConstraint.activate([
+                   loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                   loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 100),
+                   loginButton.widthAnchor.constraint(equalToConstant: 200),
+                   loginButton.heightAnchor.constraint(equalToConstant: 50)
+               ])
     }
+                   
+   func addGradientToButton(button: UIButton) {
+           let gradientLayer = CAGradientLayer()
+           gradientLayer.colors = [UIColor(red: 1, green: 0.8, blue: 0, alpha: 1).cgColor,
+                                   UIColor(red: 1, green: 0.6, blue: 0, alpha: 1).cgColor]
+           gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+           gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+           gradientLayer.frame = button.bounds
+           gradientLayer.cornerRadius = button.layer.cornerRadius
+           button.layer.insertSublayer(gradientLayer, at: 0)
+       }
 
 
 }
