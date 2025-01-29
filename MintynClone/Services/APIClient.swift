491288399c5c01913902ce20804c76b887aa86bd
decodedfaith <komolafefaith@gmail.com>
2025-01-30 00:00:01 +0100

    -Added services file scripts

---
+++ MintynClone/Services/APIClient.swift
@@ -0,0 +1,7 @@
+//
+//  APIClient.swift
+//  MintynClone
+//
+//  Created by user on 1/29/25.
+//
+//  Handles API requests
---
+++ MintynClone/Services/AuthServices.swift
@@ -0,0 +1,7 @@
+//
+//  AuthServices.swift
+//  MintynClone
+//
+//  Created by user on 1/29/25.
+//
+//  Handles login/logout
